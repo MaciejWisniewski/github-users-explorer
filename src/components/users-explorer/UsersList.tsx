2fd741fba9ade1c8c8ee3512e3d3ef3e2b689227
maciejwlocalize <maciejw@localize.city>
2024-10-06 20:22:07 +0200

    Handle no results and react-query error cases

--- src/components/users-explorer/UsersList.tsx
+++ src/components/users-explorer/UsersList.tsx
@@ -3,6 +3,11 @@ import searchService from 'services/searchService';
 import { useQuery } from '@tanstack/react-query';
 import CircularProgress from '@mui/material/CircularProgress';
 import UserCard from 'components/users-explorer/UserCard';
+import { Alert, styled } from '@mui/material';
+
+const SectionStyled = styled('section')`
+  margin: 1rem;
+`;
 
 type UsersListProps = {
   searchQuery: string;
@@ -15,17 +20,37 @@ const UsersList: React.FC<UsersListProps> = ({ searchQuery }) => {
     refetchOnWindowFocus: false,
   });
 
+  const loading = searchUsersQuery.isLoading;
+
+  const displayList =
+    !loading &&
+    searchUsersQuery.isSuccess &&
+    searchUsersQuery.data?.items?.length;
+
+  const noResults = !loading && searchUsersQuery.data?.items?.length === 0;
+
   return (
-    <section>
-      {searchUsersQuery.isLoading && <CircularProgress />}
-      {searchUsersQuery.isSuccess && searchUsersQuery.data?.items?.length ? (
+    <SectionStyled>
+      {displayList ? (
         <>
           {searchUsersQuery.data.items.map((user) => (
             <UserCard login={user.login} avatarUrl={user.avatar_url} />
           ))}
         </>
       ) : null}
-    </section>
+      {loading ? <CircularProgress /> : null}
+      {noResults ? (
+        <Alert variant="filled" severity="info">
+          No users found.
+        </Alert>
+      ) : null}
+      {searchUsersQuery.isError ? (
+        <Alert
+          variant="filled"
+          severity="error"
+        >{`There was an error: ${searchUsersQuery.error.message}`}</Alert>
+      ) : null}
+    </SectionStyled>
   );
 };
 
